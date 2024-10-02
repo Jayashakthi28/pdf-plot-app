5802631370d43a92ce6b872683b4ff7f6eb1e120
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2024-10-02 12:32:14 +0530

    Add. user login with google using firebase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,18 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { Provider } from 'react-native-paper';
+import { createContext, useEffect, useState } from 'react';
+import 'react-native-reanimated';
+import React from 'react';
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { Provider } from "react-native-paper";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import React from "react";
-import {
-  PaperProvider,
   DefaultTheme as PaperDefaultTheme,
-  MD3DarkTheme as PaperDarkTheme,
-} from "react-native-paper";
-import { Colors } from "@/constants/Colors"; // Your custom color file
-import { useColorScheme } from "@/hooks/useColorScheme"; // Your hook for detecting system theme
+} from 'react-native-paper';
+import { Colors } from '@/constants/Colors'; 
+import { useColorScheme } from '@/hooks/useColorScheme';
+import LoginScreen from '@/components/LoginScreen';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import axios from 'axios';
 
 const CustomDefaultTheme = {
   ...PaperDefaultTheme,
@@ -26,22 +22,16 @@ const CustomDefaultTheme = {
   },
 };
 
-const CustomDarkTheme = {
-  ...PaperDarkTheme,
-  colors: {
-    ...PaperDarkTheme.colors,
-    ...Colors.dark,
-  },
-};
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+export const UserContext = createContext(null);
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const theme =
-    colorScheme === "dark" ? CustomDefaultTheme : CustomDefaultTheme;
+  const theme = colorScheme === 'dark' ? CustomDefaultTheme : CustomDefaultTheme;
+  const [userDetails, setUserDetails]: any = useState();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
@@ -50,24 +40,54 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  const signinWithBackend = async (fireBaseJson: FirebaseAuthTypes.User) => {
+    const res = await axios.post(`${process.env.EXPO_PUBLIC_BACKEND_URL}/auth`, {
+      name: fireBaseJson.displayName,
+      email: fireBaseJson.email,
+    });
+    return res.data;
+  };
+
+  async function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    if (user) {
+      const backendUserDetail: any = await signinWithBackend(user);
+      setUserDetails({
+        firebaseData: user,
+        id: backendUserDetail._id,
+      });
+    } else {
+      setUserDetails(null);
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
   if (!loaded) {
     return null;
   }
 
   return (
     <Provider theme={theme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="map/[map]/index"
-          options={{
-            headerShown: true,
-            headerTitle: "",
-            headerBackTitleVisible: false,
-          }}
-        />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <UserContext.Provider value={userDetails}>
+        {!userDetails && <LoginScreen />}
+        {userDetails && (
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="map/[map]/index"
+              options={{
+                headerShown: true,
+                headerTitle: '',
+                headerBackTitleVisible: false,
+              }}
+            />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        )}
+      </UserContext.Provider>
     </Provider>
   );
 }
